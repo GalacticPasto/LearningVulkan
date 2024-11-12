3675cf519072a9111554f9a1a01b11537b67017d
GalacticPast <adhikarybrizein2003@gmail.com>
2024-11-12 16:43:16 -0500

    graphics pipeline

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 DEFINES="-std=c++17 -O2"
 LDFLAGS="-lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi"
 
+glslc src/shader.vert -o build/vert.spv
+glslc src/shader.frag -o build/frag.spv
+
 g++ $CFLAGS src/main.cpp -o build/VulkanTest $LDFLAGS
 
