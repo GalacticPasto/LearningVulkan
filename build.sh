22cd1ad66a5c769714e0dde03f8f7f3c5cacd60a
galacticPast <adhikarybrizein2003@gmail.com>
2024-11-13 20:52:42 -0500

    first triangle

--- build.sh
+++ build.sh
@@ -3,6 +3,17 @@
 DEFINES="-std=c++17 -O2"
 LDFLAGS="-lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi"
 
+set echo on 
+if [ -f "build/VulkanTest" ]; then 
+    rm -rf build/VulkanTest    
+fi 
+if [ -f "build/frag.spv" ]; then 
+    rm -rf build/frag.spv    
+fi 
+if [ -f "build/vert.spv" ]; then 
+    rm -rf build/vert.spv    
+fi 
+
 glslc src/shader.vert -o build/vert.spv
 glslc src/shader.frag -o build/frag.spv
 
