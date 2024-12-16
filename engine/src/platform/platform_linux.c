6bdc76047b44ca3ea235fd8f1cc842cca1f0fc35
GalacticPasto <182835725+GalacticPasto@users.noreply.github.com>
2024-12-16 16:58:16 -0500

    basic wayland backend

--- engine/src/platform/platform_linux.c
+++ engine/src/platform/platform_linux.c
@@ -66,7 +66,7 @@ b8 platform_startup(platform_state *plat_state, const char *application_name, i3
 
     // Loop through screens using iterator
     xcb_screen_iterator_t it = xcb_setup_roots_iterator(setup);
-    int                   screen_p = 0;
+    i32                   screen_p = 0;
     for (i32 s = screen_p; s > 0; s--)
     {
         xcb_screen_next(&it);
@@ -523,37 +523,180 @@ keys translate_keycode(u32 x_keycode)
     {                                                                                                                  \
         DASSERT(expr != 0);                                                                                            \
     }
-
+//
+#include "wayland/xdg-shell-client-protocol.h"
 #include <wayland-client.h>
+//
+// TODO: this is temporary migrate the shared memory to use vulkan instead of mannually allocating buffers
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-typedef struct internal_state
+/* Shared memory support code */
+static void randname(char *buf)
 {
-    struct wl_display    *display;
-    struct wl_compositor *compositor;
-    struct wl_surface    *surface;
-} internal_state;
-
-static void registry_handle_global(void *data, struct wl_registry *registry, uint32_t name, const char *interface,
-                                   uint32_t version)
-{
-    DINFO("interface: '%s', version: %d, name: %d", interface, version, name);
-
-    internal_state *state = (internal_state *)data;
-
-    if (strcmp(interface, wl_compositor_interface.name) == 0)
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    long r = ts.tv_nsec;
+    for (int i = 0; i < 6; ++i)
     {
-        state->compositor = wl_registry_bind(registry, name, &wl_compositor_interface, 4);
+        buf[i] = 'A' + (r & 15) + (r & 16) * 2;
+        r >>= 5;
     }
 }
 
-static void registry_handle_global_remove(void *data, struct wl_registry *registry, uint32_t name)
+static int create_shm_file(void)
 {
-    // This space deliberately left blank
+    int retries = 100;
+    do
+    {
+        char name[] = "/wl_shm-XXXXXX";
+        randname(name + sizeof(name) - 7);
+        --retries;
+        int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL, 0600);
+        if (fd >= 0)
+        {
+            shm_unlink(name);
+            return fd;
+        }
+    } while (retries > 0 && errno == EEXIST);
+    return -1;
 }
 
-static const struct wl_registry_listener registry_listener = {
-    .global = registry_handle_global,
-    .global_remove = registry_handle_global_remove,
+static int allocate_shm_file(size_t size)
+{
+    int fd = create_shm_file();
+    if (fd < 0)
+        return -1;
+    int ret;
+    do
+    {
+        ret = ftruncate(fd, size);
+    } while (ret < 0 && errno == EINTR);
+    if (ret < 0)
+    {
+        close(fd);
+        return -1;
+    }
+    return fd;
+}
+
+/* Wayland code */
+typedef struct internal_state
+{
+    /* Globals */
+    struct wl_display    *wl_display;
+    struct wl_registry   *wl_registry;
+    struct wl_shm        *wl_shm;
+    struct wl_compositor *wl_compositor;
+    struct xdg_wm_base   *xdg_wm_base;
+    /* Objects */
+    struct wl_surface   *wl_surface;
+    struct xdg_surface  *xdg_surface;
+    struct xdg_toplevel *xdg_toplevel;
+} internal_state;
+
+static void wl_buffer_release(void *data, struct wl_buffer *wl_buffer)
+{
+    /* Sent by the compositor when it's no longer using this buffer */
+    wl_buffer_destroy(wl_buffer);
+}
+
+static const struct wl_buffer_listener wl_buffer_listener = {
+    .release = wl_buffer_release,
+};
+
+static struct wl_buffer *draw_frame(struct internal_state *state)
+{
+    const int width = 1200, height = 720;
+    int       stride = width * 4;
+    int       size = stride * height;
+
+    int fd = allocate_shm_file(size);
+    if (fd == -1)
+    {
+        return NULL;
+    }
+
+    uint32_t *data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (data == MAP_FAILED)
+    {
+        close(fd);
+        return NULL;
+    }
+
+    struct wl_shm_pool *pool = wl_shm_create_pool(state->wl_shm, fd, size);
+    struct wl_buffer   *buffer = wl_shm_pool_create_buffer(pool, 0, width, height, stride, WL_SHM_FORMAT_XRGB8888);
+    wl_shm_pool_destroy(pool);
+    close(fd);
+
+    // black
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            data[y * width + x] = 0x00000000;
+        }
+    }
+
+    munmap(data, size);
+    wl_buffer_add_listener(buffer, &wl_buffer_listener, NULL);
+    return buffer;
+}
+
+static void xdg_surface_configure(void *data, struct xdg_surface *xdg_surface, uint32_t serial)
+{
+    struct internal_state *state = data;
+    xdg_surface_ack_configure(xdg_surface, serial);
+
+    // TODO: reconfigure to make use of vulkan
+
+    struct wl_buffer *buffer = draw_frame(state);
+    wl_surface_attach(state->wl_surface, buffer, 0, 0);
+    wl_surface_commit(state->wl_surface);
+}
+
+static const struct xdg_surface_listener xdg_surface_listener = {
+    .configure = xdg_surface_configure,
+};
+
+static void xdg_wm_base_ping(void *data, struct xdg_wm_base *xdg_wm_base, uint32_t serial)
+{
+    xdg_wm_base_pong(xdg_wm_base, serial);
+}
+
+static const struct xdg_wm_base_listener xdg_wm_base_listener = {
+    .ping = xdg_wm_base_ping,
+};
+
+static void registry_global(void *data, struct wl_registry *wl_registry, uint32_t name, const char *interface,
+                            uint32_t version)
+{
+    internal_state *state = data;
+    if (strcmp(interface, wl_shm_interface.name) == 0)
+    {
+        state->wl_shm = wl_registry_bind(wl_registry, name, &wl_shm_interface, 1);
+    }
+    else if (strcmp(interface, wl_compositor_interface.name) == 0)
+    {
+        state->wl_compositor = wl_registry_bind(wl_registry, name, &wl_compositor_interface, 4);
+    }
+    else if (strcmp(interface, xdg_wm_base_interface.name) == 0)
+    {
+        state->xdg_wm_base = wl_registry_bind(wl_registry, name, &xdg_wm_base_interface, 1);
+        xdg_wm_base_add_listener(state->xdg_wm_base, &xdg_wm_base_listener, state);
+    }
+}
+
+static void registry_global_remove(void *data, struct wl_registry *wl_registry, uint32_t name)
+{
+    /* This space deliberately left blank */
+}
+
+static const struct wl_registry_listener wl_registry_listener = {
+    .global = registry_global,
+    .global_remove = registry_global_remove,
 };
 
 b8 platform_startup(platform_state *plat_state, const char *application_name, i32 x, i32 y, i32 width, i32 height)
@@ -561,24 +704,30 @@ b8 platform_startup(platform_state *plat_state, const char *application_name, i3
     plat_state->internal_state = malloc(sizeof(internal_state));
     internal_state *state = (internal_state *)plat_state->internal_state;
 
-    state->display = wl_display_connect(NULL);
-    CHECK_WL_RESULT(state->display);
-    DINFO("succefully connected to wl display");
+    state->wl_display = wl_display_connect(NULL);
+    CHECK_WL_RESULT(state->wl_display);
 
-    struct wl_registry *registry = wl_display_get_registry(state->display);
-    CHECK_WL_RESULT(registry);
-    DINFO("succefully registered a wl reigster");
+    state->wl_registry = wl_display_get_registry(state->wl_display);
+    CHECK_WL_RESULT(state->wl_registry);
+    wl_registry_add_listener(state->wl_registry, &wl_registry_listener, state);
 
-    wl_registry_add_listener(registry, &registry_listener, state);
-    wl_display_roundtrip(state->display);
+    wl_display_roundtrip(state->wl_display);
 
-    state->surface = wl_compositor_create_surface(state->compositor);
-    CHECK_WL_RESULT(state->surface);
-    DINFO("succefully connected a wl surface");
+    state->wl_surface = wl_compositor_create_surface(state->wl_compositor);
+    CHECK_WL_RESULT(state->wl_surface);
+
+    state->xdg_surface = xdg_wm_base_get_xdg_surface(state->xdg_wm_base, state->wl_surface);
+    CHECK_WL_RESULT(state->xdg_surface);
+    xdg_surface_add_listener(state->xdg_surface, &xdg_surface_listener, state);
+
+    state->xdg_toplevel = xdg_surface_get_toplevel(state->xdg_surface);
+    CHECK_WL_RESULT(state->xdg_toplevel);
+    xdg_toplevel_set_title(state->xdg_toplevel, application_name);
+
+    wl_surface_commit(state->wl_surface);
 
     return true;
 }
-
 void platform_shutdown(platform_state *plat_state)
 {
 }
@@ -587,9 +736,13 @@ b8 platform_pump_messages(platform_state *plat_state)
 {
     internal_state *state = (internal_state *)plat_state->internal_state;
 
+    while (wl_display_dispatch(state->wl_display))
+    {
+        // TODO: dispatch events
+    }
+
     return true;
 }
-
 #endif
 
 void *platform_allocate(u64 size, b8 aligned)
